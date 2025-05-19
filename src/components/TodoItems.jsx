443444e7ce35eef50236725d91d510102be52489
Mouhamad-gaye <gayemouhamad280@gmail.com>
2025-05-19 12:33:20 -0600

    todo input and items

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,9 +1,23 @@
-export default function TodoItems({}) {
+export default function TodoItems({ todos, editId, editText, setEditId, setEditText, dispatch }) {
     return (
 
         <>
-           
-            
+            <li>
+                <input text="text" value={editText} onChange={(e) => setEditText(e.target.value)}>
+                <button onClick={() => {
+                        dispatch({ type: "Edit", payload: { id: todos.id, task: editText } })
+                        setEditId(null)
+                        setEditText("")
+                }}>Save</button>
+                </input>
+                <input type="checkbox" checked={todos.completed} onChange={() => dispatch({ type: "Toggle", payload: todos.id })}></input>
+                <button onClick={() => {
+                    setEditId(todos.id)
+                    setEditText(todos.task)
+                }}>Edit</button>
+                <button onClick={() => dispatch({ type: "Delete", payload: todos.id })}>Delete</button>
+            </li>
+
         </>
     )
 }
